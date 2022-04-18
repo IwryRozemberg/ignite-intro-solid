5e30420554c29561b7bfd2553d96d3257f34e280
Iwry Rozemberg <iwryrozemberg@gmail.com>
2022-04-18 00:42:13 -0300

    Incluindo documentação da api

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerDocuments from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
-
+app.use("/api-docs", swaggerUi.serve);
+app.get("/api-docs", swaggerUi.setup(swaggerDocuments));
 app.use("/users", usersRoutes);
 
 export { app };
